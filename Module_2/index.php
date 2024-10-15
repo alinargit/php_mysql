aa2ba0689f5514348a4866b8f72fd74bf006a0fd
alinargit <ramadani.alinaa@gmail.com>
2024-10-15 19:22:11 +0200

    Create index.php

---
+++ Module_2/index.php
@@ -0,0 +1,26 @@
+<?
+echo "Hello World";
+$a = 'Digital';
+$b = 'School';
+
+$c = $a.$b;
+
+echo "$c \n";
+
+//String Functions
+
+$the_string = 'Digital School';
+
+echo strlen("$the_string \n");
+
+$programming = 'Programming is not cool';
+
+echo str_replace("not", "very", $programming);
+
+$another_string = 'Programming';
+
+echo strrev($another_string);
+
+// PHP Variables Scope: Local, Global & Static
+
+?> 
