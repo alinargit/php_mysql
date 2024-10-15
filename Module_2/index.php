620ac5d74cb3411443061afd64dc93d9df7d96ab
alinargit <ramadani.alinaa@gmail.com>
2024-10-15 19:13:17 +0200

    #

---
+++ Module_2/index.php
@@ -0,0 +1,22 @@
+<?
+    $a = 'Digital';
+    $b = 'School';
+
+    $c = $a,$b;
+
+    echo "$c \n";
+
+    //String Functions
+
+    $the_string = 'Digital School';
+    echo strlen(string: "$the_string\n");
+
+    $programming = 'Programming is not cool';
+    echo str_replace(search:"not", replace:"very", subject: $programming)
+
+    $another_string = 'Programming';
+    echo strrev(string: $another_string)
+
+    // PHP Variables Scope: Local, Global & Static
+
+?>
