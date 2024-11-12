ae1e15cdc9e57b7fddbd28003247ef9a048e080b
alinargit <ramadani.alinaa@gmail.com>
2024-11-12 18:17:29 +0100

    part 2

--- Module_7/index.php
+++ Module_7/index.php
@@ -1,20 +1,35 @@
-<?
-    //Setting MySQL database parameters
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
+<?php
+//Setting MySQL database parameters
+//$host = 'localhost';
+//$user = 'root';
+//$pass = '';
 
-    //Connection in database using PDO
-    //Beginning of a try block
-    try{
-        //Attempt to create a neq PDO object and connecting to a MySQL database
-        //The connection string is contructed using the variables $host, $user and $pass
+//Connection in datatbase using PDO
+//Beinning of a try block
+//try{
+    //Attemt to create a neq PDO object and connecting to a MySQL database
+    //The connection string is contructed using the variables $host, $user and $pass
 
-        $conn = new PDO("mysql:host=$host; $user, $pass");
+  //  $conn = new PDO("mysql:host=$host, $user, $pass");
 
-        //If the connection is succesful
-        echo "Connected";
-    } catch (Exeption $e){
-        echo "Not Connected";
-    }
-?>
+    //If the connection is succesful
+ //   echo "Connected";
+//} catch (Exception $e){
+ //   echo "Not Connected";
+//}
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+
+try{
+    
+
+  $conn = new PDO("mysql:host=$host", $user, $pass);
+
+    //If the connection is succesful
+    echo "Connected";
+} catch (Exception $e){
+    echo "Not Connected";
+}
+?> 
