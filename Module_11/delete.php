a8959941dd306ae2bba96099721da818dfaf1ec1
alinargit <ramadani.alinaa@gmail.com>
2024-12-10 19:19:04 +0100

    hi

---
+++ Module_11/delete.php
@@ -0,0 +1,18 @@
+<?php
+include 'db.php';
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM users WHERE id = :id";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+    if($stmt->execute()){
+        echo "User deleted successfully";
+    }
+    else{
+        echo "Error deleting the user";
+    }
+}
+?>
